6912422859be0afa1a6f22f19a3083ff8b338aeb
Fandy Nuary <geraldferaldy@Gerald-Feraldys-MacBook-Pro.local>
2015-09-09 15:15:09 +0700

    First unit test and url mapping, dummy view

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
+# from django.contrib import admin
+from lists import views
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'superlists.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.home_page, name='home'),
 ]
