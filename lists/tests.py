7ea2efdaee65fe1881206ef2e37aee542c04f03c
Fandy Nuary <geraldferaldy@Gerald-Feraldys-MacBook-Pro.local>
2015-09-09 15:53:51 +0700

    create homepage

--- lists/tests.py
+++ lists/tests.py
@@ -16,4 +16,6 @@ class HomePageTest(TestCase):
         response = home_page(request)
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertIn(b'<head>Zwesty Tria Sabila</head>', response.content)
+        self.assertIn(b'<body>1206208403</body>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
