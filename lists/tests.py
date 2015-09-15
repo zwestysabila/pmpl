a094ec0a9bffa3e69e52235c8bc326befab1759c
Fandy Nuary <geraldferaldy@Gerald-Feraldys-MacBook-Pro.local>
2015-09-16 02:38:34 +0700

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,4 @@
+from django.template.loader import render_to_string
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
@@ -14,8 +15,10 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertIn(b'<head>Zwesty Tria Sabila<br></head>', response.content)
         self.assertIn(b'<body>1206208403</body>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        self.assertTrue(response.content.strip().endswith(b'</html>'))
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title><head>Zwesty Tria Sabila<br></head><body>1206208403</body></html>')
+    return render(request, 'home.html')
