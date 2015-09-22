ba342c162a2ea4a9acc522496a0daa1f33b61abf
Fandy Nuary <geraldferaldy@Gerald-Feraldys-MacBook-Pro.local>
2015-09-22 21:17:02 +0700

    chapter 6 ch06l031

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,4 @@
-from lists.models import Item
+from lists.models import Item, List
 from django.template.loader import render_to_string
 from django.core.urlresolvers import resolve
 from django.test import TestCase
@@ -7,7 +7,6 @@ from django.http import HttpRequest
 from lists.views import home_page
 
 class HomePageTest(TestCase):
-
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -28,8 +27,9 @@ class HomePageTest(TestCase):
         self.assertIn('yey, waktunya berlibur', response.content.decode())
 
     def test_home_page_displays_comments_less_five(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         request = HttpRequest()
         response = home_page(request)
@@ -39,11 +39,12 @@ class HomePageTest(TestCase):
         self.assertIn('sibuk tapi santai', response.content.decode())
 
     def test_home_page_displays_comments_greater_equal_five(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
-        Item.objects.create(text='itemey 3')
-        Item.objects.create(text='itemey 4')
-        Item.objects.create(text='itemey 5')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
+        Item.objects.create(text='itemey 3', list=list_)
+        Item.objects.create(text='itemey 4', list=list_)
+        Item.objects.create(text='itemey 5', list=list_)
 
         request = HttpRequest()
         response = home_page(request)
@@ -78,16 +79,23 @@ class NewListTest(TestCase):
         )
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
-class ItemModelTest(TestCase):
-
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+        
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -95,7 +103,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 
@@ -104,8 +114,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
     
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
