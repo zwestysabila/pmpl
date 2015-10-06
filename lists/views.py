b2fb20b6f84587934acda827a3353306c2725cbb
zwesty <zwestyts@gmail.com>
2015-10-06 17:20:07 +0700

    perbaiki comment chapter 6. ch6-done.

--- lists/views.py
+++ lists/views.py
@@ -4,25 +4,28 @@ from lists.models import Item, List
 
 
 # Create your views here.
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    countList = Item.objects.filter(list_id=list_.id).count()
+    if countList == 0 :
+        comment = 'yey, waktunya berlibur'
+    if (countList < 5) and (countList != 0) :
+        comment = 'sibuk tapi santai'
+    if countList >= 5 :
+        comment = 'oh tidak'
+    return render(request, 'list.html', {'list': list_, 'comment':comment})
+   
+def home_page(request):
+    #if request.method == 'POST':
+     #   Item.objects.create(text=request.POST['item_text'])
+      #  return redirect('/lists/the-only-list-in-the-world/')  
+    comment = 'yey, waktunya berlibur'   
+    return render(request, 'home.html', {'comment':comment})
+
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/%d/' % (list_.id,))
-
-def view_list(request, list_id):
-   list_ = List.objects.get(id=list_id)
-   return render(request, 'list.html', {'list': list_})
-
-def home_page(request):
-   #Items = Item.objects.all()
-   countsItem = Item.objects.count()
-   comment = 'yey, waktunya berlibur'
-   
-   if countsItem > 0:
-       comment = 'sibuk tapi santai'
-   if countsItem >= 5:
-       comment = 'oh tidak'
-   return render(request, 'home.html', {'comment' : comment})
 
 def new_list(request):
    list_ = List.objects.create()
