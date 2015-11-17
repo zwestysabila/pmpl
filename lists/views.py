95331d2801df6ef75f1d198d340fd8235dadb93b
zwesty <zwestyts@gmail.com>
2015-11-17 16:04:10 +0700

    Adjust new list view to do model validation

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
+from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from lists.models import Item, List
-
 
 # Create your views here.
 def view_list(request, list_id):
@@ -14,12 +14,15 @@ def view_list(request, list_id):
     if countList >= 5 :
         comment = 'oh tidak'
     return render(request, 'list.html', {'list': list_, 'comment':comment})
-   
+
 def home_page(request):
     #if request.method == 'POST':
-     #   Item.objects.create(text=request.POST['item_text'])
-      #  return redirect('/lists/the-only-list-in-the-world/')
-    return render(request, 'index.html')
+    #   Item.objects.create(text=request.POST['item_text'])
+    #  return redirect('/lists/the-only-list-in-the-world/')
+    #return render(request, 'home.html')
+    countsItem = Item.objects.count()
+    comment = 'yey, waktunya berlibur'   
+    return render(request, 'home.html', {'comment':comment})
 
 def list_page(request):
     countsItem = Item.objects.count()
@@ -32,6 +35,13 @@ def add_item(request, list_id):
     return redirect('/lists/%d/' % (list_.id,))
 
 def new_list(request):
-   list_ = List.objects.create()
-   Item.objects.create(text=request.POST['item_text'], list=list_)    
-   return redirect('/lists/%d/' % (list_.id,))
+    list_ = List.objects.create()
+    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    try:    
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {"error": error})
+    return redirect('/lists/%d/' % (list_.id,))
--- superlists/urls.py
+++ superlists/urls.py
@@ -4,7 +4,7 @@ from lists import urls as list_urls  #2
 
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
-    url(r'^homelist', list_views.list_page, name='list'),
+    #url(r'^homelist', list_views.list_page, name='list'),
     url(r'^lists/', include(list_urls)),
     # url(r'^admin/', include(admin.site.urls)),
 ]
