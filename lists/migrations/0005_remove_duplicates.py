057b41f3b7137dbf5f51efe337a05efaa19eadb5
zwesty <zwestyts@gmail.com>
2015-11-18 17:28:27 +0700

    appendix D

---
+++ lists/migrations/0005_remove_duplicates.py
@@ -0,0 +1,25 @@
+# encoding: utf8
+from django.db import models, migrations
+
+def find_dupes(apps, schema_editor):
+    List = apps.get_model("lists", "List")
+    for list_ in List.objects.all():
+        items = list_.item_set.all()
+        texts = set()
+        for ix, item in enumerate(items):
+            if item.text in texts:
+                item.text = '{} ({})'.format(item.text, ix)
+                item.save()
+            texts.add(item.text)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lists', '0004_auto_20150922_1406'),
+    ]
+
+    operations = [
+        migrations.RunPython(find_dupes),
+    ]
+
---
+++ lists/migrations/0006_unique_together.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lists', '0005_remove_duplicates'),
+    ]
+
+    operations = [
+    ]
